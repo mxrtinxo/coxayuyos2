7803e07c34e324be951f4845916588231867fdd6
mxrtinxo <164051055+mxrtinxo@users.noreply.github.com>
2024-11-06 18:10:49 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,40 @@
 import { Routes } from '@angular/router';
+import { loginGuard } from './guards/login.guard';
+import { homeGuard } from './guards/home.guard';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'ingreso',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'map',
+    loadComponent: () => import('./pages/map/map.page').then( m => m.MapPage),
+  },
+  {
+    path: 'theme',
+    loadComponent: () => import('./pages/theme/theme.page').then( m => m.ThemePage)
+  },
+  {
+    path: 'ingreso',
+    loadComponent: () => import('./pages/ingreso/ingreso.page').then( m => m.IngresoPage),
+    canActivate: [homeGuard]
+  },
+  {
+    path: 'correo',
+    loadComponent: () => import('./pages/correo/correo.page').then( m => m.CorreoPage)
+  },
+  {
+    path: 'pregunta',
+    loadComponent: () => import('./pages/pregunta/pregunta.page').then( m => m.PreguntaPage)
+  },
+  {
+    path: 'correcto',
+    loadComponent: () => import('./pages/correcto/correcto.page').then( m => m.CorrectoPage)
+  },
+  {
+    path: 'incorrecto',
+    loadComponent: () => import('./pages/incorrecto/incorrecto.page').then( m => m.IncorrectoPage)
   },
 ];
---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,78 @@
+import { IonIcon, IonToolbar } from '@ionic/angular/standalone';
+import { Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { ViewWillEnter } from '@ionic/angular';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LanguageComponent } from 'src/app/components/language/language.component';
+import { Router } from '@angular/router';
+import { colorWandOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+import { AuthService } from 'src/app/services/auth.service';
+import { IonContent, IonHeader, IonTitle, IonInput, IonButton, IonRow, IonCol, IonGrid, IonCard, IonItem, 
+IonCardContent, IonCardHeader, IonFooter } from '@ionic/angular/standalone';
+import { User } from 'src/app/model/user';
+
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: true,
+  imports: [CommonModule,  IonRow, IonCol, IonFooter, TranslateModule, IonGrid, IonIcon, IonItem, IonCardContent, 
+    IonCardHeader, FormsModule, IonTitle, IonToolbar, IonCard, IonInput, IonButton, 
+    IonContent, IonHeader, LanguageComponent] 
+})
+export class LoginPage implements ViewWillEnter {
+
+  /** 
+   * CGV-INI-Traducciones
+   * Para poder utilizar la traducción de textos, se debe:
+   *   1. Ejecutar: npm i @ngx-translate/core 
+   *   2. Ejecutar: npm i @ngx-translate/http-loader
+   *   3. Crear carpeta: src/app/assets/i18n
+   *   4. Crear archivo: src/app/assets/i18n/es.json para los textos en español
+   *   5. Crear archivo: src/app/assets/i18n/en.json para los textos en inglés
+   * 
+   * CGV-FIN-Traducciones
+  */ 
+
+  @ViewChild('selectLanguage') selectLanguage!: LanguageComponent;
+
+  correo: string;
+  password: string;
+
+  constructor(
+      private router: Router
+    , private translate: TranslateService
+    , private authService: AuthService) 
+  { 
+    this.correo = 'atorres';
+    this.password = '1234';
+    // Los iconos deben ser agregados a uno (ver en https://ionic.io/ionicons)
+    addIcons({ colorWandOutline }); 
+  }
+
+  async ionViewWillEnter() {
+    this.selectLanguage.setCurrentLanguage();
+  }
+
+  navigateTheme() {
+    this.router.navigate(['/theme']);
+  }
+
+  login() {
+    this.authService.login(this.correo, this.password);
+  }
+
+  registerNewUser() {
+
+  }
+
+  passwordRecovery() {
+    
+  }
+
+}
